d04b336ce1a81ec4679f41a2341bd41b3aa94012
sbarrow57 <sbarrow57@gmail.com>
2014-08-09 17:46:14 +0100

    First random forest submission. Predicting only count.

--- Scripts/main.R
+++ Scripts/main.R
@@ -1,10 +1,12 @@
 #Load Packages
 library("data.table")
+library("plyr")
 library("dplyr")
 library("ggplot2")
 library("lubridate")
-library("caret")
 library("stringr")
+library("caret")
+library("randomForest")
 
 #Set WD
 setwd("~/R/Kaggle/Bike Sharing/")
@@ -17,13 +19,33 @@ source("Scripts/model.R")
 train=read.csv("Data/Raw Data/train.csv")
 test = read.csv("Data/Raw Data/test.csv")
 
+#get training output
+count=train$count
+
+#get test datetime
+dt=test$datetime
+
 #Complete data preparation
 train=dataPrep(train)
 test=dataPrep(test) 
 
+train[,datetime:=NULL]
+train[,casual:=NULL]
+train[,registered:=NULL]
+train[,date:=NULL]
+
+test[,datetime:=NULL]
+test[,date:=NULL]
+
+dummies<-dummyVars(count~.,data=train)
+dummyTest<-dummyVars(~.,data=test)
+
+train<-data.table(predict(dummies,train,na.action=na.omit))
+test<-data.table(predict(dummytest,test,na.action=na.omit))
+
 #Create model and predict values
-predictedCount<-predictCount(train,test)
+predictedCount<-predictCount(train,count,test)
 
 #Create output
-output = data.frame(datetime=test$datetime,count=predictedCount)
+output = data.frame(datetime=dt,count=predictedCount)
 write.csv(output,"Data/Processed Data/output.csv",row.names=F)
--- Scripts/model.R
+++ Scripts/model.R
@@ -1,11 +1,10 @@
-predictCount<-function(train,test){
+predictCount<-function(train,count,test){
   
-  #Poisson model for casual and registered user seperately
-  regFit<-glm(registered ~ season+holiday+workingday+weather+temp+atemp+humidity+windspeed+time,family="poisson",data=train)
-  casFit<-glm(casual ~ season+holiday+workingday+weather+temp+atemp+humidity+windspeed+time,family="poisson",data=train)
+  #Random forest model based on 12 variable selection
+  rfFit<-randomForest(x=train, y=count,mtry=12)
   
   
   #Make predictions using models
-  predict(regFit,newdata = test,type='response')+predict(casFit,newdata = test,type='response')
+  predict(rfFit,newdata = test)
   
 }
